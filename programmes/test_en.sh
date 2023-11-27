b200efea6f30a9029dc2dd06ade11093673c6375
Cong Jinyu <congtaotao2001@gmail.com>
2023-11-27 15:06:13 +0100

    mod_anglais

--- programmes/test_en.sh
+++ programmes/test_en.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
-# taper "bash programme/miniprojet.sh urls/fr.txt" pour exécuter ce programme
-counter=1
-urlfile=$1
 
-if [ $# -ne 1 ]
-then
-	echo "Un argument attendu exactement." # Si l'argument existe
-	exit
-else
-	if [ -f $urlfile ] 
-	then
-		echo "on a bien un fichier"
-	else
-		echo "on attend un fichier qui existe"
-		exit
-	fi
-fi
+counter=1
+urlfile=../urls/anglais.txt
 
 
 echo "<html>
@@ -24,17 +10,27 @@ echo "<html>
 	</head>
 	<body>
 		<table border="black solid">
-			<tr><th>numéro_url</th><th>url</th><th>status_code</th><th>charset</th>" > table_anglais.html
+			<tr><th>numéro_url</th><th>url</th><th>status_code</th><th>charset</th><th>compte</th>" > ../tableaux/tableau-en.html
 
 
 while read -r line;
 do
 	if [ -n "$line" ];
 	then
-		lang=$(basename $URLS .txt)
+		echo "en traitement de l'url numéro $counter"
+		lang=$(basename $urlfile .txt)
 		code=$(curl -I -L -s -w "%{http_code}" -o /dev/null $line)
 		charset=$(curl -I -s -w "%{content_type}" -o /dev/null $line | grep -P -o "charset=\S+" | cut -d "=" -f2)
-		echo -e "\t\t\t<tr><td>$counter</td><td>$line</td><td>$code</td><td>$charset</td></tr>" >> table_anglais.html
+		# aspirations
+		curl $line > ../aspirations/fich-$lang-$counter.html
+		# dump text
+		lynx --dump --nolist $line > ../dumps-text/fich-$lang-$counter.txt
+		# occurence de mot étudié
+		occurence=$(lynx --nolist --dump $line | grep -i transgender | wc -l)
+		# récupérer les contextes
+		lynx --nolist --dump $line | grep -i -B 1 -A 1 "transgender" > ../contextes/fich-$lang-$counter.txt
+		# remplie le tableau
+		echo -e "\t\t\t<tr><td>$counter</td><td>$line</td><td>$code</td><td>$charset</td><td>$occurence</td></tr>" >> ../tableaux/tableau-en.html
 		counter=$(expr $counter + 1)
 	fi
 done < $urlfile
@@ -42,4 +38,4 @@ done < $urlfile
 
 echo "		</table>
 	</body>
-</html>" >> table_anglais.html
+</html>" >> ../tableaux/tableau-en.html
