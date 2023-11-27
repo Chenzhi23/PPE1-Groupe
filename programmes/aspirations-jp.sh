dae436c37bc50a4b788b5ffe0c2e338a859d9fbe
yuchieh0621mumu <113918568+yuchieh0621mumu@users.noreply.github.com>
2023-11-27 22:07:51 +0100

    typo

--- programmes/aspirations-jp.sh
+++ programmes/aspirations-jp.sh
@@ -8,6 +8,6 @@ ligne=0
 while read -r line
 do
     ligne=$( expr $ligne + 1 )
-    curl -o ./../aspirations/fr/file$ligne-jp.html $line
+    curl -o ./../aspirations/jp/file$ligne-jp.html $line
 done < $input_liens
 
