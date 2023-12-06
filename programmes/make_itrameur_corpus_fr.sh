a2e756eb1a25475c6cb9f509dbb0a11ea4efce6b
Chenzhi SUN <chenzhisun@wifi-64-5-151.wifi.univ-paris3.fr>
2023-12-06 09:56:02 +0100

    rename urls

--- programmes/make_itrameur_corpus_fr.sh
+++ programmes/make_itrameur_corpus_fr.sh
@@ -18,8 +18,9 @@ echo "<lang=\"$lang\">">$chemin_output
 for nb in {1..50}
 #将 for 循环的范围用 {1..50} 替换了 range(1, 51)。在 Bash 中，可以使用花括号表示法来生成数字范围。
 do
-    fichier=./../$dossier/$lang/$(basename ../$dossier"s-text"/$lang/fich$nb-$lang.txt)
+    fichier=./../$dossier"s-text"/$lang/$(basename ../$dossier"s-text"/$lang/fich$nb-$lang.txt)
     contenu=$(cat $fichier | sed -E 's/&/&amp;/g' | sed -E 's/</&lt;/g' | sed -E 's/>/&gt;/g')
+    # sed 后面需要接 单引号 而不是双引号，否则会什么来着？     
     #sed一直报错sed: RE error: illegal byte sequence
     echo "<page=\"$lang-$nb\">
 <text>
