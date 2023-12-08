df6970aeda70489d336904061b9c0cdd2aaab8f8
Chenzhi SUN <chenzhisun23@gmail.com>
2023-12-08 12:36:51 +0100

    maj contextes-fr

--- programmes/contextes-fr.sh
+++ programmes/contextes-fr.sh
@@ -8,6 +8,6 @@ ligne=0
 while read -r line
 do
     ligne=$( expr $ligne + 1 )
-    cat ./../dumps-text/fr/fich$ligne-fr.txt | ggrep -P -A 2 -B 2 "(T|t)ransgenres?" > ./../contextes/fr/fich$ligne-fr.txt
+    cat ./../dumps-text/fr/fich$ligne-fr.txt | ggrep -P -a -A 2 -B 2 "(T|t)ransgenres?" > ./../contextes/fr/fr-$ligne.txt
 done < $input_liens
 
