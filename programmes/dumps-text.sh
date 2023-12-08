1890d62a2ec4c809b9a3bd36a93387a662aaa8bf
Chenzhi SUN <chenzhisun23@gmail.com>
2023-12-08 21:37:52 +0100

    maj dump-fr

--- programmes/dumps-text.sh
+++ programmes/dumps-text.sh
@@ -8,6 +8,6 @@ ligne=0
 while read -r line
 do
     ligne=$( expr $ligne + 1 )
-    lynx -dump -nolist $line > ./../dumps-text/fr/fich$ligne-fr.txt
+    lynx -dump -nolist $line > ./../dumps-text/fr/fr-$ligne.txt
 done < $input_liens
 
