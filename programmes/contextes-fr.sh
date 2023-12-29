7d3077a2f5d39a3a6962db6134aa3c30789ae41e
Chenzhi SUN <chenzhisun23@gmail.com>
2023-12-29 16:53:37 +0100

    maj

--- programmes/contextes-fr.sh
+++ programmes/contextes-fr.sh
@@ -4,10 +4,33 @@
 input_liens=$1
 
 ligne=0
+lang=$(basename $1 .txt)
+
+if [ $lang = "fr" ]; 
+then
+	cible="(T|t)ransgenres?"
+fi
+ 
+if [ $lang = "en" ]; 
+then
+	cible="transgender"
+fi
+
+if [ $lang = "ch" ]; 
+then
+	cible="跨性别"
+fi
+
+if [ $lang = "jp" ]; 
+then
+	cible="トランスジェンダー"
+fi
+
+
 
 while read -r line
 do
     ligne=$( expr $ligne + 1 )
-    cat ./../dumps-text/fr/fich$ligne-fr.txt | ggrep -P -a -A 2 -B 2 "(T|t)ransgenres?" > ./../contextes/fr/fr-$ligne.txt
+    cat ./../dumps-text/$lang/$lang-$ligne.txt | ggrep -P -a -A 2 -B 2 "$cible" > ./../contextes/$lang/$lang-$ligne.txt
 done < $input_liens
 
