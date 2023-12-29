682e59232b22776c9680a60f6865dafec6f22a43
Chenzhi SUN <chenzhisun23@gmail.com>
2023-12-29 14:30:40 +0100

    maj script tableaux

--- programmes/test_fr.sh
+++ programmes/test_fr.sh
@@ -15,6 +15,28 @@ chemin=./../tableaux/$lang.html
 ## 也可以把 chemin 变成argument (chemin=$2)
 
 
+if [ $lang = "fr" ]; 
+then
+	cible="(T|t)ransgenres?"
+fi
+ 
+if [ $lang = "en" ]; 
+then
+	cible="transgender"
+fi
+
+if [ $lang = "ch" ]; 
+then
+	cible="跨性别"
+fi
+
+if [ $lang = "jp" ]; 
+then
+	cible="トランスジェンダー"
+fi
+
+
+
 ## 可以 echo "
 #               "
 echo "<html>
@@ -24,21 +46,24 @@ echo "<html>
 
 echo "</head>
 <body>
-<table>
+<table border='1'>
 <tr><th>Ligne</th><th>URL</th><th>Code HTTP</th><th>Encodage</th><th>Compte</th><th>Contexte</th><th>Aspirations</th><th>Dump</th></tr>" >> $chemin
 
 fichier_urls=$1
 ligne=0
+
 while read -r line
 ### 为什么最后一行不被read啊？
 do
     ligne=$( expr $ligne + 1 )
     code=$(curl -s -I -L -w  "%{http_code}" -o /dev/null $line)
-    charset=$(curl -s -I -L -w  "%{content_type}" -o /dev/null $line ｜ ggrep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
+    #charset=$(curl -s -I -L -w  "%{content_type}" -o /dev/null $line ｜ ggrep -P -o "charset=[^;]*" | cut -d"=" -f2 | tail -n 1)
+    charset=$(curl -s -I -L $line | grep -i 'Content-Type' | ggrep -oP 'charset=\K([-A-Za-z0-9]+)')
+
     ###################################################################我的mac`grep -P`行不通的，至于为什么此处结果正确，因为删掉`-P`也可以
-    compte=$(cat ./../dumps-text/fr/fr-$ligne.txt | ggrep -o -P "(T|t)ransgenres?" | wc -l)
+    compte=$(cat ./../dumps-text/$lang/$lang-$ligne.txt | ggrep -o -P "$cible" | wc -l)
     echo "<tr>
-    <td>$ligne</td><td>$line</td><td>$code</td><td>$charset</td><td>$compte</td><td><a href="./../contextes/fr/fr-$ligne.txt">Contexte$ligne</a></td><td><a href="./../aspirations/fr/fr-$ligne.html">Aspiration$ligne</a></td><td><a href="./../dumps-text/fr/fr-$ligne.txt">Dump$ligne</a></td>
+    <td>$ligne</td><td>$line</td><td>$code</td><td>$charset</td><td>$compte</td><td><a href="./../contextes/$lang/$lang-$ligne.txt">Contexte$ligne</a></td><td><a href="./../aspirations/$lang/$lang-$ligne.html">Aspiration$ligne</a></td><td><a href="./../dumps-text/$lang/$lang-$ligne.txt">Dump$ligne</a></td>
     </tr>" >> $chemin
 
 done < $fichier_urls
